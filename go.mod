56807ff5d9ac032cb01fe94abffe8c6754cca55f
Ian Carr <ian.carr@gmail.com>
2020-05-11 16:46:39 -0600

    added logging framework

--- go.mod
+++ go.mod
@@ -1,4 +1,7 @@
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/sirupsen/logrus v1.6.0
+	github.com/urfave/cli/v2 v2.2.0
+)
 
 module github.com/icarr/bitfluxr
 
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
@@ -14,7 +14,16 @@ var (
 )
 
 func main() {
+	initLogging()
+	app := initCLI()
 
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initCLI() *cli.App {
 	app := &cli.App{
 		Name:    "BitFluxR",
 		Version: "0.0.1",
@@ -25,15 +34,52 @@ func main() {
 			},
 		},
 		Copyright: "(c) 2020 BitFluxR",
-		Usage:     "Tame all hte bits",
-		Action: func(c *cli.Context) error {
-			fmt.Println("send me the bits")
-			return nil
+		Usage:     "Tame all the bits",
+		//Flags
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "verbose",
+				Aliases: []string{"vv"},
+				Usage:   "verbose output",
+			},
+			&cli.StringFlag{
+				Name:    "config, c",
+				Aliases: []string{"c"},
+				Usage:   "load config from `FILE`",
+			},
+			&cli.StringFlag{
+				Name: "log",
+			},
+		},
+		//Commands
+		Commands: []*cli.Command{
+			{
+				Name: "add",
+				//Aliases: []string{"a"},
+				Usage: "add data source",
+				Action: func(c *cli.Context) error {
+					fmt.Println("added source: ", c.Args().First())
+					return nil
+				},
+			},
+			{
+				Name: "complete",
+				//Aliases: []string{"c"},
+				Usage: "complete a task on the list",
+				Action: func(c *cli.Context) error {
+					fmt.Println("completed task: ", c.Args().First())
+					return nil
+				},
+			},
 		},
 	}
+	return app
+}
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+func initLogging() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
 }
